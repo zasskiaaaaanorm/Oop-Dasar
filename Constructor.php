e2c3c05ae498376731da225d9580c45988a66db5
saskia <saskia.norma@koding.sch.id>
2019-09-13 13:31:05 +0700

    featur constructor

---
+++ Constructor.php
@@ -0,0 +1,35 @@
+<?php
+
+class Produk
+{
+    public $judul,
+           $penulis,
+           $penerbit,
+           $harga;
+
+    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0)
+    {
+        $this->judul = $judul;
+        $this->penulis = $penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
+    }
+
+    public function getLabel()
+    {
+        return "$this->penulis, $this->penerbit";
+    }
+
+}
+
+$produk1 = new Produk("Mekorama", "Mekorima", "Hayam Wuruk", 50000);
+$produk2 = new Produk("Mobile legend", "Bang Bang", "Legendaris", 45000);
+$produk3 = new Produk("Dragon Ball");
+
+
+
+echo "Komik : " . $produk1->getLabel();
+echo "<br>";
+echo "Game : " . $produk2->getLabel();
+echo "<br>";
+var_dump($produk3);
